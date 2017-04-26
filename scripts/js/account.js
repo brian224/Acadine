c3971f7299501d87bb03a7afe0bd4fb9722d1666
Brian Lin <brianlin224@gmail.com>
2017-04-26 20:36:42 +0800

    working on account page

--- scripts/js/account.js
+++ scripts/js/account.js
@@ -4,8 +4,9 @@
 	var pageObj = new page();
 
 	function page() {
-		this._category = '.jQ-category';
-		this._curr     = '.jQ-curr';
+		this._category   = '.jQ-category';
+		this._curr       = '.jQ-curr';
+		this._datepicker = '.jQ-datepicker';
 	}
 
 	projects.$w.load(function(){
@@ -13,10 +14,14 @@
 
 	projects.$d.ready(function(){
 		$(pageObj._curr).text($('.category-list .is-curr').text());
+		if ($(pageObj._datepicker).length !== 0) {
+			$(pageObj._datepicker).DatePicker();
+		}
 
 		$(pageObj._category).on('click', function(){
 			if (projects.$w.width() < 740) {
 				$(this).addClass('is-active');
+				$(common._sudMenu).removeClass('is-active');
 				common.offClick(pageObj._category);
 			}
 		});
